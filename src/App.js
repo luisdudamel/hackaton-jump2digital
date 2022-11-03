7eee8821a9ac05d51c231da864f2617ac60d32a0
Luis Dudamel <ladusa@gmail.com>
2022-11-03 15:54:55 +0100

    Initial button skeleton added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import Button from "./components/Button/Button";
+
 const App = () => {
-  return <></>;
+  return <Button></Button>;
 };
 
 export default App;
---
+++ src/components/Button/Button.js
@@ -0,0 +1,5 @@
+const Button = () => {
+  return <Button>asdsad</Button>;
+};
+
+export default Button;
